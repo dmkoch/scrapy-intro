680e063facc80ed2702d4c945aa0c8b6fe9cd8fd
Daniel Koch <dkoch@mm.st>
2013-09-25 11:26:47 -0700

    add sample pipeline class and settings

--- newsbot/newsbot/pipelines.py
+++ newsbot/newsbot/pipelines.py
@@ -2,7 +2,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.exceptions import DropItem
 
-class NewsbotPipeline(object):
+
+class MissingFieldsPipeline(object):
     def process_item(self, item, spider):
+        for field in ('headline', 'url', 'article'):
+            print field
+            if not item.get(field, False):
+                raise DropItem("Missing '%s' in %s" % (field, item))
+
         return item
--- newsbot/newsbot/settings.py
+++ newsbot/newsbot/settings.py
@@ -12,4 +12,14 @@ SPIDER_MODULES = ['newsbot.spiders']
 NEWSPIDER_MODULE = 'newsbot.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'newsbot (+http://www.yourdomain.com)'
+USER_AGENT = 'newsbot (+http://www.meetup.com/python-charlotte/)'
+
+# Run every item through these pipelines
+ITEM_PIPELINES = [
+    'newsbot.pipelines.MissingFieldsPipeline',
+]
+
+# Crawl politely
+CONCURRENT_REQUESTS_PER_DOMAIN = 2
+DOWNLOAD_DELAY = 2.0
+RANDOMIZE_DOWNLOAD_DELAY = True
